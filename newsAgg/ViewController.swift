5245bdca05e8ab8a6a094b0985fc32df1c1fdc28
CyrilusK <kcyrg17@gmail.com>
2024-10-28 12:51:00 +0300

    initial project

---
+++ newsAgg/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  newsAgg
+//
+//  Created by Cyril Kardash on 28.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+
+
+}
+
